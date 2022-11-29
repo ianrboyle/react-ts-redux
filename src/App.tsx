c98cbec0497069cc58ac33f1f30dede703611ac7
Ian Boyle <irboyle21@gmail.com>
2022-11-29 11:02:28 -0500

    adds carvalue

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import './App.css';
 import { CarForm } from './components/CarForm';
 import { CarList } from './components/CarList';
 import { CarSearch } from './components/CarSearch';
+import { CarValue } from './components/CarValue';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <CarForm />
       <CarSearch />
       <CarList />
+      <CarValue />
     </div>
   );
 }
--- src/components/CarValue.tsx
+++ src/components/CarValue.tsx
@@ -1,7 +1,25 @@
 import React from 'react'
-
+import {useSelector} from 'react-redux'
+import { IState } from '../store'
 export const CarValue = () => {
+
+  const carValue = useSelector((state: IState) =>{
+    if (state.cars.cars){
+      const searchTerm = state.cars.searchTerm;
+      const cars = state.cars.cars.filter((car) => car.name?.toLocaleLowerCase().includes(searchTerm.toLocaleLowerCase()));
+      let carSum = 0;
+       cars.forEach(c => {
+        if (c.cost){
+          carSum += c.cost
+        }
+        });
+        return carSum;
+      }
+    
+  })
   return (
-    <div>CarValue</div>
+    <div><h3>
+      Car Value: {carValue}</h3>
+      </div>
   )
 }
