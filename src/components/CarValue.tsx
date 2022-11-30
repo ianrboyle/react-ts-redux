dc153fd345711bdddc828f27c7fd29bf4b7885d6
Ian Boyle <irboyle21@gmail.com>
2022-11-29 20:22:31 -0500

    modifies reduce function

--- src/components/CarValue.tsx
+++ src/components/CarValue.tsx
@@ -7,17 +7,14 @@ export const CarValue = () => {
   const carValue = useSelector((state: IState) =>{
     if (state.cars.cars){
       const searchTerm = state.cars.searchTerm;
-      const cars = state.cars.cars.filter((car) => car.name?.toLocaleLowerCase().includes(searchTerm.toLocaleLowerCase()));
-
+      const cars = state.cars.cars.filter((car) => car.name?.toLocaleLowerCase().includes(searchTerm.toLocaleLowerCase()))
+      if (cars.length > 0){
         return cars.reduce((acc, car) => {
-          if (car.cost) {
-            return acc + car.cost
-          }
-          return acc
+          return acc + car.cost
         }, 0)
       }
-    
-  })
+    } });
+  
   return (
     <div><h3>
       Car Value: {carValue}</h3>
